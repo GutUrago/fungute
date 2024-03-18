f29b4446cd6e10b7c6f68b4192729e054b4e00da
GutUrago <girjagutama@gmail.com>
2024-03-19 00:21:33 +0100

    Initial commit

---
+++ R/survey_csv.R
@@ -0,0 +1,38 @@
+
+
+
+#' Reads csv data file
+#'
+#' @param data is a csv data frame to be read
+#' @param label an excel file containing names and labels of variables
+#' @param sht specify sheet name if excel has more than one sheet
+#' @param fct if you wish to convert strings in to factors
+#'
+#' @return cleaned data
+#' @export
+#'
+#' @examples
+#' # survey_csv("test.csv")
+#'
+
+
+
+
+survey_csv <- function(data,
+                       label = NULL,
+                       sht = NULL,
+                       fct = TRUE){
+        if(fct){
+          dt <- data.table::fread(input = data,
+                                stringsAsFactors = fct)
+        } else {
+                dt <- data.table::fread(input = data)
+        }
+        if(!is.null(label) && !is.null(sht)){
+                label <- readxl::read_excel(path = label, sheet = sht)
+        } else if(!is.null(label)){
+                label <- readxl::read_excel(path = label)
+        }
+
+
+}
